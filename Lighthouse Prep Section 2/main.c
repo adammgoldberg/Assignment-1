86363337b4743fee4789ebe9bcee97e383102583
Adam Goldberg <adammgoldberg@hotmail.com>
2015-09-12 17:27:52 -0400

    Initial Commit

---
+++ Lighthouse Prep Section 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lighthouse Prep Section 2
+//
+//  Created by Adam Goldberg on 2015-09-12.
+//  Copyright (c) 2015 Adam Goldberg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
